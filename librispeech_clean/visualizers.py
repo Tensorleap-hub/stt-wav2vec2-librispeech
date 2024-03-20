40c45b718af010222059592a3169c60f86588d9b
Danielle Ben Bashat <daniellebenba@gmail.com>
2024-03-20 13:11:07 +0200

    text mask vis

--- librispeech_clean/visualizers.py
+++ librispeech_clean/visualizers.py
@@ -53,6 +53,10 @@ def display_mel_spectrogram_heatmap(data: npt.NDArray[np.float32]) -> np.ndarray
 
 
 def display_waveform(data: npt.NDArray[np.float32]) -> LeapGraph:
+    sr = config.get_parameter('sampling_rate')
+    sr_target = sr // 10
+    downsampled_signal = resample(data, orig_sr=sr, target_sr=sr_target)
+    resized_data = downsampled_signal[:config.get_parameter('clip_visualizers')]
     # data_trimmed = remove_trailing_zeros(data)
     resized_data = data[:config.get_parameter('clip_visualizers')]
     resized_data = resized_data.reshape(-1, 10).mean(1)
@@ -74,7 +78,8 @@ def display_waveform_heatmap(data: npt.NDArray[np.float32]) -> np.ndarray:
     return res
 
 
-def vis_alignments(prediction: np.ndarray, numeric_labels: np.ndarray) -> LeapTextMask:
+
+def vis_alignments_pred(prediction: np.ndarray, numeric_labels: np.ndarray) -> LeapTextMask:
     """
     Returns: LeapTextMask Vis
     mask: npt.NDArray[np.uint8]: length of text with labels in each index
@@ -82,27 +87,40 @@ def vis_alignments(prediction: np.ndarray, numeric_labels: np.ndarray) -> LeapTe
     labels: List[str]
     type: LeapDataType = LeapDataType.TextMask
     """
-    numeric_labels = remove_trailing_zeros(numeric_labels[0])
+    numeric_labels = remove_trailing_zeros(numeric_labels)
     processor = ProcessorSingleton().get_processor()
-    processed_pred = np.argmax(prediction, 2)[0]
+    processed_pred = np.argmax(prediction, 1)
     transcription = processor.decode(processed_pred)
     reference = processor.tokenizer.decode(numeric_labels)
 
     character_process_out = process_characters(reference, transcription)
-    text = list(reference)
+    text = list(transcription)
     mask = np.zeros(len(text), dtype=np.uint8)
-    labels = ["-", "hit"]
+    labels = ["-", "equal", "insert", "deletion", "substitute", "delete"]
 
     alignments = character_process_out.alignments
     for align in alignments[0]:
-        if align.type == "equal":
-            start, end = align.ref_start_idx, align.ref_end_idx
-            # align.hyp_start_idx, align.hyp_end_idx
-            mask[start: end] = 1
+        align_type = align.type
+        # start, end = align.ref_start_idx, align.ref_end_idx
+        start, end = align.hyp_start_idx, align.hyp_end_idx
+        mask[start: end] = labels.index(align_type)
 
-        # todo: add also insertion, deletion sub?
-
+    length = config.get_parameter('text_visualizers')
+    if len(text) < length:
+        p = (length-len(text))
+        text += [""]*p
+        mask = np.concatenate([mask, np.zeros(p, dtype=np.uint8)])
     return LeapTextMask(mask, text, labels)
 
 
-# todo: add also ofr prediction to show heatmaps
+
+def vis_alignments_heatmap(data: npt.NDArray[np.float32]) -> np.ndarray:
+    data = np.squeeze(data)
+    length = config.get_parameter('text_visualizers')
+    resized_data = np.zeros(length)
+    if len(data) > length:
+        data = data[:length+1]
+    resized_data[0:len(data)] = data
+    return resized_data
+
+
